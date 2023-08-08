946d884f3dc824da4f04328bdadd5c9fcf556a2e
OguzhanOksuz <oguzhanoksuz0@gmail.com>
2023-08-08 18:28:41 +0300

    string_utils.c eklendi

--- src/parser/parse.c
+++ src/parser/parse.c
@@ -6,7 +6,7 @@
 /*   By: mkaraden <mkaraden@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 17:48:01 by mkaraden          #+#    #+#             */
-/*   Updated: 2023/08/07 11:37:38 by ooksuz           ###   ########.fr       */
+/*   Updated: 2023/08/08 17:26:45 by ooksuz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 void	parse_cub(t_game *game, int ac, char **av)
 {
-	t_data *data = malloc(sizeof(t_data));
-	
+	t_data	*data;
+
+	data = malloc(sizeof(t_data));
 	if (!data)
 		ft_error(ERR_MALLOC, game);
 	if (ac == 2)
@@ -23,6 +24,7 @@ void	parse_cub(t_game *game, int ac, char **av)
 		if (ft_strcmp(av[1] + ft_strrchr(av[1]) + 1, ".cub") == 0)
 			ft_error(ERR_FILE, game);
 		map_check(data, av[1]);
+		init_map(data, av[1]);
 	}
 	else
 		ft_error(ERR_AC, game);
--- src/parser/string_utils.c
+++ src/parser/string_utils.c
@@ -1,36 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   map_check.c                                        :+:      :+:    :+:   */
+/*   string_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ooksuz <ooksuz@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/07 11:35:42 by ooksuz            #+#    #+#             */
-/*   Updated: 2023/08/08 17:58:27 by ooksuz           ###   ########.fr       */
+/*   Created: 2023/08/08 18:22:11 by ooksuz            #+#    #+#             */
+/*   Updated: 2023/08/08 18:26:10 by ooksuz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
-#include "libs.h"
-
-void	map_check(t_data *game, char *src)
+char	*ft_strrchr(char *str, char ch)
 {
-	int		fd;
-	int		i;
-	char	*tmp;
-	char	**raw_map;
+	int	i;
 
-	fd = open(src, O_RDONLY);
-	if (fd < 0)
-		ft_error(ERR_FILE, game);
 	i = 0;
-	tmp = get_next_line(fd);
-	while (tmp)
+	while (str[i])
 	{
-		free (tmp);
-		tmp = get_next_line(fd);
 		i++;
 	}
-	printf ("i = %d", i);
-	check_contents(game, raw_map);
+	while (i >= 0 && str[i] != ch)
+		i--;
+	return (str + i);
 }
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
